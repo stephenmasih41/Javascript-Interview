216c862983ca624cd513b98cb05ce6e7005c68d8
stephen249 <stephenmasih39@gmail.com>
2025-06-27 23:48:59 -0400

    Day 2 Eight Questions

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ function panic(str) {
 }
 
 // Test your function
-console.log(panic("I'm almost out of coffee"));
-console.log(panic("winter is coming"));
-console.log(panic("Hello"));
+// console.log(panic("I'm almost out of coffee"));
+// console.log(panic("winter is coming"));
+// console.log(panic("Hello"));
 
 /*Problem 2:- 
 Whispering function 
@@ -61,5 +61,357 @@ function whisper(sentence) {
   return "shh... " + lower;
 }
 
-console.log(whisper("PLEASE STOP SHOUTING."));
-console.log(whisper("MA'AM, this is a Wendy's!"));
+// console.log(whisper("PLEASE STOP SHOUTING."));
+// console.log(whisper("MA'AM, this is a Wendy's!"));
+
+/* 
+Problem 3:-
+Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
+
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+*/
+
+function altCaps(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (i % 2 === 0) {
+      newStr += char.toUpperCase();
+    } else {
+      newStr += char.toLowerCase();
+    }
+  }
+  return newStr; // Return the built string after the loop
+}
+
+// console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
+/* toTitleCase
+Write a function that will capitalize every word in a sentence.  
+
+Example Input: "everything, everywhere, all at once"
+Example Output: "Everything, Everywhere, All At Once"
+*/
+
+/* 
+Problem 4:-
+First, write a function that takes in one word and 
+capitalizes the first letter of that word.
+
+Example Input: "scrimba"
+Example Output: "Scrimba"
+
+Hint: Trying using slice() and .toUpperCase()
+*/
+
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+/* 
+Now write a function that capitalizes every word in a sentence. 
+How can you reuse the function you just wrote? 
+*/
+
+function toTitleCase(str) {
+  return str
+    .split(" ")
+    .map((word) => capitalizeWord(word))
+    .join(" ");
+}
+
+// Test your functions
+// console.log(capitalizeWord("pumpkin"));
+// console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
+
+/* 
+Problem 5:-
+Totally Not Another FizzBuzz 
+
+Scrimba CEO Per Borgen wants you to write a program to grant special bonuses to all his employees based on their employee ID numbers! 
+
+Scrimba has 100 employees and their employee ID numbers range from 1 - 100. If the employee's ID number is: 
+
+Divisible by 3 - Vacation! 
+Divisible by 5 - $100,000 bonus! 
+Divisible by both 3 and 5 - JACKPOT! 1 Million and a Yacht!
+Not divisible by 3 or 5 - :(
+    
+Write a program to loop through all the ID numbers and print their prize. 
+Your function's output should look something like this: 
+
+1 - :(
+2 - :(
+3 - Vacation! 
+4 - :(
+5 - $100,000 bonus!
+
+Hint: Remainder operator, modulo 
+ */
+
+function awardBonuses() {
+  const range = [];
+  for (let i = 1; i <= 100; i++) {
+    range.push(i);
+  }
+  range.map((num) => {
+    if (num % 5 === 0 && num % 3 === 0) {
+      console.log(`${num} - JACKPOT! 1 Million and a Yacht!`);
+    } else if (num % 3 === 0) {
+      console.log(` ${num} - Vacation!`);
+    } else if (num % 5 === 0) {
+      console.log(`${num} - $100,000 bonus!`);
+    } else {
+      console.log(`${num} - :(`);
+    }
+  });
+}
+
+awardBonuses();
+
+/*  Emojify!
+
+Popular services like Slack and Github allow for emoji shortscodes, meaning 
+they will detect when a word in a sentence begins and ends with a colon (:)
+and automatically replace that word with an emoji. 
+
+These shortcodes allow users to add an emoji to their messages by typing a 
+code rather than searching for an emoji from a list. 
+
+For example, typing :smile: will replace that text with 😊 
+
+*/
+
+const emojis = {
+  smile: "😊",
+  angry: "😠",
+  party: "🎉",
+  heart: "💜",
+  cat: "🐱",
+  dog: "🐕",
+};
+
+/* 
+Problem 6:-
+1. Write a function that checks if a lowercase word starts and 
+ends with a colon. If it does, remove the colons and
+look up the word in the emoji object. If the word is in the 
+emojis object, return the corresponding emoji.
+If it isn't, return the original word.
+
+Example input: ":party:"
+Example output: 🎉
+
+Example input: ":flower:"
+Example output: "flower"
+
+Example input: "elephant"
+Example output: "elephant"
+*/
+
+function emojifyWord(word) {
+  if (word.endsWith(":") && word.startsWith(":")) {
+    const name = word.slice(1, -1);
+    return emojis[name] || name;
+  } else {
+    return word;
+  }
+}
+
+/* 2. Write a function to find any emoji shortcodes in a phrase.
+Your function should map over each word in the phrase, emojify any word
+that begins and ends with a colon, then return the emojified phrase. 
+Feel free to use your emojify function from the previous exercise!
+
+Example input: "I :heart: my :cat:"
+Example output: "I 💜 my 🐱"
+
+Example input: "I :heart: my elephant"
+Example output: "I 💜 my elephant"
+*/
+
+function emojifyPhrase(phrase) {
+  return phrase
+    .split(" ")
+    .map((word) => emojifyWord(word))
+    .join(" ");
+}
+
+// console.log(emojifyWord(":heart:"));
+// console.log(emojifyWord(":flower:"));
+// console.log(emojifyWord("elephant"));
+
+// console.log(emojifyPhrase("I :heart: my :cat:"));
+// console.log(emojifyPhrase("I :heart: my :elephant:"));
+
+/* 
+Problem 7:-
+Anagrams are groups of words that can be spelled with the same letters. 
+For example, the letters in "pea" can be rearrange to spell "ape", and 
+the letters in "allergy" can be rearranged to spell "gallery."
+
+Write a function to check if two strings of lowercase letters are anagrams. 
+Return true if the word is an anagram. Return false if it isn't. 
+
+Example input: "allergy", "gallery"
+Example output: true
+
+Example input: "rainbow", "crossbow"
+Example output: false
+
+*/
+function sorting(str) {
+  return str.split("").sort().join("");
+}
+function isAnagram(str1, str2) {
+  const logic = sorting(str1) === sorting(str2);
+  const len = str1.length === str2.length;
+  if (logic && len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isAnagram("allergy", "gallery"));
+// console.log(isAnagram("treasure", "measure"));
+
+/* We Come in Peace!  
+We've received what (we assume) is a message of peace and brotherhood from 
+an alien planet. They almost got it right, but the messages are 
+backward. Write functions to reverse the backward messages so we can 
+read what they have to say! 
+*/
+
+const title = ":htraE no od ot ffutS";
+const messages = [
+  "maerc eci yrT",
+  "rewoT leffiE tisiV",
+  "noom eht ot snamuh etacoleR",
+  "egrahc ni stac tuP",
+];
+
+/* 
+Problem 8:-
+Step 1: Reverse a string
+Write a function that takes in a string and returns the reverse 
+of that string. An interviewer may want to check if you know your
+string methods, or may want to know if you can reverse a string manually. 
+Practice both ways! 
+
+Example input: !htrae ot emocleW
+Example output: Welcome to earth!
+*/
+
+function reverseString(arr) {
+  const split = arr.split(" ");
+  const reversing = split.map((word) => {
+    return word.split("").reverse().join("");
+  });
+  return reversing.join(" ");
+}
+
+/*
+Step 2: Now we'll reverse all strings in an array. Write a function that takes in
+an array of strings and returns a new array with all strings reversed.
+
+You can use reuse your reverseString() function, use string methods, or 
+reverse the strings manually. 
+*/
+
+function reverseStringsInArray(arr) {
+  return messages.map((word) => reverseString(word));
+}
+
+// console.log(reverseString(title));
+// console.log(reverseStringsInArray(messages));
+
+/* 
+Problem 9:-
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palindrome, return true. If it isn't, return false.
+
+Example input: "motorbike"
+Example output: false
+
+Example input: "rotator" 
+Example output: true
+*/
+function reverse(str) {
+  return str.split("").reverse().join("");
+}
+function isPalindrome(str) {
+  if (str === reverse(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Test your function
+// console.log(isPalindrome("abba"));
+// console.log(isPalindrome("civic"));
+// console.log(isPalindrome("octopus"));
+// console.log(isPalindrome("pumpkins"));
+// console.log(isPalindrome("madam"));
+
+/*  
+Problem 10:-
+Grandpa's hand isn't as steady as it used to be. You finally convinced him
+to start using a password manager, but he accidentally typed and saved his
+password with a bunch of extra characters. Help him recover his password by 
+removing all the duplicate characters. 
+
+Your function should take in a string of characters and return a
+string with the duplicate characters removed. Assume that your input
+is lowercased with only letters and numbers.  
+
+Example input: "aabbccb1212"
+Example output: "abc12"
+*/
+const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23";
+
+function removeDupeChars(chars) {
+  let result = "";
+  for (let char of chars) {
+    if (!result.includes(char)) {
+      result += char;
+    }
+  }
+  return result;
+}
+
+// console.log(removeDupeChars(password));
+
+/* 
+Problem 11:-
+How often do the letters in your name repeat? 
+
+Write a function that counts how many times each letter of your name
+occurs. Your function should take in your first and last name and return
+an object where the keys are each character in your name, and the value
+is how many times that character appears in your name. 
+
+Example input: "Peggy Porth"
+Example output: {p: 2, e: 1, g: 2, y: 1, o: 1, r: 1, t: 1, h: 1}
+
+Your function should NOT count spaces and should not be case sensitive (a
+lowercase t and a capital T should be considered the same character).
+
+*/
+
+function countChars(str) {
+  const result = {};
+  const low = str.toLowerCase().split(" ").join("");
+  for (let char of low) {
+    result[char] = (result[char] || 0) + 1;
+  }
+  return result;
+}
+
+// console.log(countChars("Peggy Porth"));
